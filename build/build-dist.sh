154d96d11b50f837e9dad2b8744735129982ca73
Piróg Piotr <pirogpiotr@gmail.com>
2025-04-11 14:12:54 +0200

    chore: Dodanie kompilacji TypeScript i SCSS do skryptu budującego

--- build/build-dist.sh
+++ build/build-dist.sh
@@ -2,7 +2,7 @@
 
 CWD=$(pwd)
 
-XPONLMS_DIR=$(dirname "$(dirname "$(dirname "$(realpath "$0")")")")
+XPONLMS_DIR=$(dirname "$(dirname "$(realpath "$0")")")
 OUTPUT_FILE=/tmp/XponLms-lastbuild.tar.gz
 OUTPUT_DIR=/tmp/xpon-plugin-build/
 
@@ -15,6 +15,13 @@ echo
 
 
 cd "$XPONLMS_DIR" || exit
+
+echo "Sass public/css"
+sass public/css || exit
+
+echo "Compiling typescript"
+tsc || exit
+
 
 DIR_TO_COPY=(
   modules
@@ -33,13 +40,14 @@ DIR_TO_COPY=(
   src/XponLmsPlugin/XponLmsPlugin.php
 );
 
+mkdir "$OUTPUT_DIR/XponLms"
+
 for i in "${DIR_TO_COPY[@]}"; do
   echo "Copy $i"
-  # shellcheck disable=SC2086
-  cp -vr --parents XponLms/$i "$OUTPUT_DIR"
+  cp -vr --parents "$i" "$OUTPUT_DIR/XponLms" || exit
 done
 
-cd $OUTPUT_DIR
+cd $OUTPUT_DIR || exit
 
 tar -zcpvf "$OUTPUT_FILE" -- .
 
